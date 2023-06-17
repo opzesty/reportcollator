1c18d4d563d85c95a8a64301412cc0a4eb57c95c
SainTfactor <sta0003@auburn.edu>
2023-06-16 22:47:53 -0400

    basic postback

--- app/basic_flask_app.py
+++ app/basic_flask_app.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/generate_form')
+@app.route('/generate_form', methods=["POST"])
 def generate_form():
-    #todo logic
-    return 'Hello, World!'
+    unit_name = request.form['unit_name']
+    am_pm = request.form['am_pm']
+    report_type = request.form['report_type']
+    filenames = ""
+    print(len(request.files))
+    for uploaded_file in request.files.getlist("files"):
+        filenames += uploaded_file.filename + " "
+    return "{} - {} - {} - {}".format(unit_name, am_pm, report_type, filenames)
 
 @app.route('/')
 def main():
